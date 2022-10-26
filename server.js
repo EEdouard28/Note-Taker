6f7b85b5c7f3b567e4a646dc4ba1cf4fd2e54c08
Evince Edouard <studioimac@evinces-imac.mynetworksettings.com>
2022-10-26 19:36:48 -0400

    port fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const htmlroutes = require('./routing/htmlroutes'); //HTML router path instance
 const apiroutes = require('./routing/apiroutes'); //API router path instance
 const app = express(); //Express js
-const PORT = process.env.PORT||3001; 
+const PORT = process.env.PORT || 3001; 
 //process.env.PORT|| checks for live server heroku and use as port otherwise use local 3001
 
 // Sets up the Express app to handle data parsing
