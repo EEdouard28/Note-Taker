60affec2d374628c7a171839808dfa41b4a78396
EEdouard28 <111817163+EEdouard28@users.noreply.github.com>
2022-10-28 12:00:40 -0400

    html route

--- server.js
+++ server.js
@@ -10,8 +10,9 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-app.use("/api", apiroutes);
 app.use("/", htmlroutes);
+app.use("/api", apiroutes);
+
 
 
 app.listen(PORT, () =>
